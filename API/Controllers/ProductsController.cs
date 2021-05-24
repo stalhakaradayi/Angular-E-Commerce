3655770f589d8fbc26c0aebd2db28a93e2245a80
stalhakaradayi <sukrutalhakaradayi@gmail.com>
2021-05-24 14:24:53 +0300

    Add project files.

---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        [HttpGet]
+        public string GetProducts()
+        {
+            return "this will be a list Products";
+        }
+        
+        [HttpGet("{id}")]
+        public string GetProduct(int id)
+        {
+            return "Single product";
+        }
+    }
+}
